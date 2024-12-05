022a2c4311ec1da813fb5d2fcfe6ae4c9f43f7c0
Gabriel Dantas <dantasbiell20@gmail.com>
2024-12-04 22:39:34 -0300

    Slider Services

--- src/Components/Sections/Projects/ProjectsElements.jsx
+++ src/Components/Sections/Projects/ProjectsElements.jsx
@@ -27,23 +27,22 @@ export const ProjectsBox = styled.section`
 export const ProjectsContainer = styled.div`
 
     display: flex;
-    justify-content: space-around;
+    justify-content: start;
     flex-wrap: wrap;
     width: 100%;
     max-width: 1200px;
     margin: auto;
     padding: 50px 0px;
+    position: relative;
 `;
 
 export const Project = styled.div`
 
-    width: 45%;
-    height: 280px;
+    width: 90%;
+    height: 550px;
+    position: relative;
     transition: 0.2s;
-    margin-bottom: 50px;
-    clip-path: polygon(0% 5%, 5% 0%, 100% 0%, 100% 95%, 95% 100%, 0% 100%);
-    border-radius: 5px;
-    border: 4px solid #01ee88;
+    border-radius: 10px;
     background-color: ${colors.base};
     cursor: pointer;
 
@@ -57,10 +56,75 @@ export const Project = styled.div`
 
         height: 320px;
     }
+`;
+
+export const SlideBox = styled.div`
+
+    width: 100%;
+    height: 100%;
+    position: relative;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+`;
+
+export const ProjectsLine = styled.div`
+
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0%;
+`;
+
+export const RowProject = styled.div`
+
+    width: ${({ $sld })=>($sld === 1 ? '90%' : '15%')};
+    height: ${({ $sld })=>($sld === 1 ? '550px' : '40%')};
+    position: absolute;
+    left: ${({ $sld })=>($sld === 2 ? '51%' : $sld === 3 ? '68%' : $sld === 4 ? '85%' : '0')};
+    z-index: ${({ $sld })=>($sld === 1 ? '0' : '3')};
+    background-color: ${({ $sld })=>($sld === 1 ? 'red' : 'yellow')};
+    border-radius: 10px;
+    border: 1px solid gray;
+    transition: 0.5s;
+`;
+
+export const LineButtons = styled.div`
+
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    z-index: 999;
+`;
+
+export const ContainerButtons = styled.div`
+
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+`;
+
+export const LineButton = styled.div`
+
+    width: 40px;
+    height: 40px;
+    color: ${colors.base};
+    background-color: ${colors.contrast_main};
+    border-radius: 10px;
+    padding: 10px;
+    margin: 0px 5px;
+    font-size: 20px;
+    opacity: 0.8;
+    cursor: pointer;
 
     &:hover {
 
-        transform: scale(1.1);
+        opacity: 1;
     }
 `;
 
--- src/Components/Sections/Projects/index.jsx
+++ src/Components/Sections/Projects/index.jsx
@@ -1,36 +1,97 @@
+import { useState } from "react";
 import TitleSection from "../../TitleSection"
 import { 
+    ContainerButtons,
     Destaque,
+    LineButton,
+    LineButtons,
     Project,
     ProjectImg,
     ProjectTitle,
     ProjectsBox, 
-    ProjectsContainer
+    ProjectsContainer,
+    ProjectsLine,
+    RowProject,
+    SlideBox
 } from "./ProjectsElements"
 
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 const Projects = () => {
+
+    const [slideArray, SetArray] = useState([1, 2, 3, 4]);
+
+
+    function moveRight() {
+
+        var number;
+        var array = [];
+
+        slideArray.forEach((id) => {
+
+            if (id === 1) {
+
+                number = 4;
+
+            } else {
+
+                number = id - 1;
+            }
+
+            array.push(number);
+        })
+
+        SetArray(array);
+    }
+
+    function moveLeft() {
+
+        var number;
+        var array = [];
+
+        slideArray.forEach((id) => {
+
+            if (id === 4) {
+
+                number = 1;
+
+            } else {
+
+                number = id + 1;
+            }
+
+            array.push(number);
+        })
+
+        SetArray(array);
+    }
 
   return (
     
     <ProjectsBox>
+
         <TitleSection text={'Projetos'}/>
+
         <ProjectsContainer>
-            <Project onClick={() => {window.open('https://luimit.com', '_blank')}}>
-                <ProjectImg img="0"/>
-                <ProjectTitle><Destaque>Site</Destaque> Luimit</ProjectTitle>
+
+            <Project>
+                <LineButtons>
+                    <ContainerButtons>
+                        <LineButton onClick={moveLeft}><FaArrowLeft/></LineButton>
+                        <LineButton onClick={moveRight}><FaArrowRight/></LineButton>
+                    </ContainerButtons>
+                </LineButtons>
             </Project>
-            <Project img="1" onClick={() => {window.open('https://www.sinergiasee.com.br', '_blank')}}>
-                <ProjectImg img="1"/>
-                <ProjectTitle><Destaque>Site</Destaque> Sinergia</ProjectTitle>
-            </Project>
-            <Project img="2" onClick={() => {window.open('https://www.ctsttreinamentosfsa.com', '_blank')}}>
-                <ProjectImg img="2"/>
-                <ProjectTitle><Destaque>Site</Destaque> CTST Treinamentos</ProjectTitle>
-            </Project>
-            <Project img="3" onClick={() => {window.open('https://b1ell20.github.io/bank/', '_blank')}}>
-                <ProjectImg img="3"/>
-                <ProjectTitle><Destaque>Site</Destaque> Bank</ProjectTitle>
-            </Project>
+
+            <ProjectsLine>
+                <SlideBox>
+                    <RowProject id="1" $sld={slideArray[0]}></RowProject>
+                    <RowProject id="2" $sld={slideArray[1]}></RowProject>
+                    <RowProject id="3" $sld={slideArray[2]}></RowProject>
+                    <RowProject id="4" $sld={slideArray[3]}></RowProject>
+                </SlideBox>
+            </ProjectsLine>
+
         </ProjectsContainer>
     </ProjectsBox>
   )
